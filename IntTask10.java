db51694679c651dd85cde7723f3e170482325797
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2018-03-15 19:41:32 +0100

    CodeAcademy

--- IntTask10.java
+++ IntTask10.java
@@ -4,9 +4,8 @@ public class IntTask10 {
 
     public static void main(String[] args) {
 
-        long someNumber = 12124321425345l; // jeśli long to L lub l na końcu
+        long someNumber = 121243214; // jeśli long to L lub l na końcu
         int someNumberTwo = (int) 123l;
-
 
         System.out.println(someNumberTwo);
     }
