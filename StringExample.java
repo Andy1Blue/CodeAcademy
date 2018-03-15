b206eee639643139d2e252ea8715c3ed7920b21c
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2018-03-15 19:35:46 +0100

    StringExample

---
+++ StringExample.java
@@ -0,0 +1,31 @@
+package pl.krzysztofbujak;
+
+import java.util.Random;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int a = 4;
+        int b = 4;
+        String aa = "Hello";
+        String bb = "Hello";
+
+        Integer c = 1;
+
+        //porównuje referncje a nie obiekty
+        if(aa.equals(bb)) {
+           System.out.println("Takie same");
+        } else {
+            System.out.println("Nie takie same");
+        }
+
+        String aaa = "gfdgd";
+        String aaa2 = "gfdggggfgdfgfd";
+        aaa = aaa.toUpperCase();
+        System.out.println(aaa);
+
+    }
+
+}
