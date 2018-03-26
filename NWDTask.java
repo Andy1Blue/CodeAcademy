e847b1e59dc0f626fd6e13056e3f71f015fdf0a2
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2018-03-26 18:49:16 +0200

    CodeAcademy

---
+++ NWDTask.java
@@ -0,0 +1,26 @@
+package pl.krzysztofbujak;
+
+public class NWDTask {
+
+    //Największy wspólny dzielnik
+    
+    public static void main(String[] args) {
+
+        NWDTask nwdTask = new NWDTask();
+
+        System.out.println(nwdTask.NWD(100, 100));
+
+    }
+
+    public int NWD(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+        return a;
+    }
+
+}
